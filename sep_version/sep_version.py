29dc8e0a9c2514c4c8378e8de948574d29bdc4c2
Shea Craig <sheagcraig@gmail.com>
2016-10-17 10:15:51 -0400

    Clean up old stuff and implement filter_machines for SEP plugin

--- sep_version/sep_version.py
+++ sep_version/sep_version.py
@@ -33,21 +33,14 @@ class SEPVersion(IPlugin):
         # or machine_group id to use (mainly for linking to the right search).
         if page == "front":
             t = loader.get_template(
-                "sheagcraig/managementcompliance/templates/front.html")
+                "sheagcraig/sep_version/templates/front.html")
         elif page in ("bu_dashboard", "group_dashboard"):
             t = loader.get_template(
-                "sheagcraig/managementcompliance/templates/id.html")
+                "sheagcraig/sep_version/templates/id.html")
 
         data = InventoryItem.objects.filter(
-            machine_in=machines, name="Symantec Endpoint Protection").values(
+            machine__in=machines, name="Symantec Endpoint Protection").values(
                 "version").annotate(count=Count("version")).order_by("version")
-        # in_compliance = self.get_in_compliance(machines).count()
-        # out_of_compliance = self.get_out_of_compliance(machines).count()
-        # unknown = machines.count() - (in_compliance + out_of_compliance)
-
-        # data = [{"label": "In Compliance", "value": in_compliance},
-        #         {"label": "Out of Compliance", "value": out_of_compliance},
-        #         {"label": "Unknown Status", "value": unknown}]
 
         c = Context({
             "title": self.get_description(),
@@ -58,35 +51,6 @@ class SEPVersion(IPlugin):
         return t.render(c)
 
     def filter_machines(self, machines, data):
-        if data == "In Compliance":
-            machines = self.get_in_compliance(machines)
-            title = "Machines in compliance"
+        machines = machines.filter(inventoryitem__name="Symantec Endpoint Protection", inventoryitem__version=data)
 
-        elif data == "Out of Compliance":
-            machines = self.get_out_of_compliance(machines)
-            title = "Machines out of compliance"
-
-        elif data == "Unknown Status":
-            machines = self.get_machines_with_no_plugin_results(machines)
-            title = "Machines which have not reported their compliance"
-
-        else:
-            machines = None
-
-        return machines, title
-
-    def get_in_compliance(self, machines):
-        return self.filter_machines_by_plugin_result(machines, True)
-
-    def get_out_of_compliance(self, machines):
-        return self.filter_machines_by_plugin_result(machines, False)
-
-    def filter_machines_by_plugin_result(self, machines, value):
-        return machines.filter(
-                pluginscriptsubmission__plugin=self.name,
-                pluginscriptsubmission__pluginscriptrow__pluginscript_name="MunkiProfilePresent",
-                pluginscriptsubmission__pluginscriptrow__pluginscript_data=value)
-
-    def get_machines_with_no_plugin_results(self, machines):
-            return machines.exclude(
-                pluginscriptsubmission__plugin=self.name)
+        return machines, "Machines with version {} of Symantec Endpoint Protection".format(data)
