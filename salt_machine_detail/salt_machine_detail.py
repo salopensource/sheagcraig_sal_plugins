faec373a04fb76f551628903b00de594c041ae91
shcrai <shcrai@macreporting.na.sas.com>
2018-10-17 15:42:35 -0400

    Add salt machine detail plugin.

---
+++ salt_machine_detail/salt_machine_detail.py
@@ -0,0 +1,15 @@
+import sal.plugin
+from server.models import PluginScriptRow
+
+
+class SaltMachineDetail(sal.plugin.DetailPlugin):
+
+    description = 'List of salt states and their statuses'
+
+    def get_context(self, machine, **kwargs):
+        context = self.super_get_context(machine, **kwargs)
+        status = (
+            PluginScriptRow.objects
+	    .filter(submission__machine=machine, submission__plugin='SaltMachineDetail'))
+        context['data'] = {s.pluginscript_name: str(s.pluginscript_data) for s in status}
+        return context
---
+++ salt_machine_detail/scripts/salt_machine_detail.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+
+import json
+import os
+import sys
+
+sys.path.append("/usr/local/sal")
+import utils
+
+
+salt_highstate_log = '/var/log/salt/events'
+
+
+def main():
+    if os.path.exists(salt_highstate_log):
+        with open(salt_highstate_log) as raw_log:
+            # The salt events won't parse as a whole. So just slice out
+            # the good stuff.
+            events = json.loads(raw_log.readlines()[-1])
+
+    result = {i['__id__']: i['result'] for i in events['return'].values()}
+
+    utils.add_plugin_results('SaltMachineDetail', result)
+
+
+if __name__ == "__main__":
+    main()
